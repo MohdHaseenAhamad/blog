c3c136c9437c97b5e8318d93f44ad1bddfb7ccb6
MohdHaseenAhamad <happyhassen0786@gmail.com>
2023-10-11 18:52:24 +0530

    home page changes

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -21,24 +21,24 @@
                     <div class="row">
                         <div class="col-xl-12">
                             <!-- form -->
-                            <form action="#" class="search-box">
-                                <div class="input-form mb-30">
-                                    <input type="text" placeholder="When Would you like to go ?">
-                                </div>
-                                <div class="select-form mb-30">
-                                    <div class="select-itms">
-                                        <select name="select" id="select1">
-                                            <option value="">When</option>
-                                            <option value="">Services-1</option>
-                                            <option value="">Services-2</option>
-                                            <option value="">Services-3</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="search-form mb-30">
-                                    <a href="#">Search</a>
-                                </div>
-                            </form>
+                            {{--<form action="#" class="search-box">--}}
+                                {{--<div class="input-form mb-30">--}}
+                                    {{--<input type="text" placeholder="When Would you like to go ?">--}}
+                                {{--</div>--}}
+                                {{--<div class="select-form mb-30">--}}
+                                    {{--<div class="select-itms">--}}
+                                        {{--<select name="select" id="select1">--}}
+                                            {{--<option value="">When</option>--}}
+                                            {{--<option value="">Services-1</option>--}}
+                                            {{--<option value="">Services-2</option>--}}
+                                            {{--<option value="">Services-3</option>--}}
+                                        {{--</select>--}}
+                                    {{--</div>--}}
+                                {{--</div>--}}
+                                {{--<div class="search-form mb-30">--}}
+                                    {{--<a href="#">Search</a>--}}
+                                {{--</div>--}}
+                            {{--</form>--}}
                         </div>
                     </div>
                 </div>
@@ -47,52 +47,52 @@
     </div>
     <!-- slider Area End-->
     <!-- Our Services Start -->
-    <div class="our-services servic-padding">
-        <div class="container">
-            <div class="row d-flex justify-contnet-center">
-                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6">
-                    <div class="single-services text-center mb-30">
-                        <div class="services-ion">
-                            <span class="flaticon-tour"></span>
-                        </div>
-                        <div class="services-cap">
-                            <h5>8000+ Our Local<br>Guides</h5>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6">
-                    <div class="single-services text-center mb-30">
-                        <div class="services-ion">
-                            <span class="flaticon-pay"></span>
-                        </div>
-                        <div class="services-cap">
-                            <h5>100% Trusted Tour<br>Agency</h5>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6">
-                    <div class="single-services text-center mb-30">
-                        <div class="services-ion">
-                            <span class="flaticon-experience"></span>
-                        </div>
-                        <div class="services-cap">
-                            <h5>28+ Years of Travel<br>Experience</h5>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6">
-                    <div class="single-services text-center mb-30">
-                        <div class="services-ion">
-                            <span class="flaticon-good"></span>
-                        </div>
-                        <div class="services-cap">
-                            <h5>98% Our Travelers<br>are Happy</h5>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    {{--<div class="our-services servic-padding">--}}
+        {{--<div class="container">--}}
+            {{--<div class="row d-flex justify-contnet-center">--}}
+                {{--<div class="col-xl-3 col-lg-3 col-md-3 col-sm-6">--}}
+                    {{--<div class="single-services text-center mb-30">--}}
+                        {{--<div class="services-ion">--}}
+                            {{--<span class="flaticon-tour"></span>--}}
+                        {{--</div>--}}
+                        {{--<div class="services-cap">--}}
+                            {{--<h5>8000+ Our Local<br>Guides</h5>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+                {{--<div class="col-xl-3 col-lg-3 col-md-3 col-sm-6">--}}
+                    {{--<div class="single-services text-center mb-30">--}}
+                        {{--<div class="services-ion">--}}
+                            {{--<span class="flaticon-pay"></span>--}}
+                        {{--</div>--}}
+                        {{--<div class="services-cap">--}}
+                            {{--<h5>100% Trusted Tour<br>Agency</h5>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+                {{--<div class="col-xl-3 col-lg-3 col-md-3 col-sm-6">--}}
+                    {{--<div class="single-services text-center mb-30">--}}
+                        {{--<div class="services-ion">--}}
+                            {{--<span class="flaticon-experience"></span>--}}
+                        {{--</div>--}}
+                        {{--<div class="services-cap">--}}
+                            {{--<h5>28+ Years of Travel<br>Experience</h5>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+                {{--<div class="col-xl-3 col-lg-3 col-md-3 col-sm-6">--}}
+                    {{--<div class="single-services text-center mb-30">--}}
+                        {{--<div class="services-ion">--}}
+                            {{--<span class="flaticon-good"></span>--}}
+                        {{--</div>--}}
+                        {{--<div class="services-cap">--}}
+                            {{--<h5>98% Our Travelers<br>are Happy</h5>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
     <!-- Our Services End -->
     <!-- Favourite Places Start -->
     {{--<div class="favourite-place place-padding">--}}
@@ -139,73 +139,114 @@
     {{--</div>--}}
     <!-- Favourite Places End -->
     <!-- Video Start Arera -->
-    <div class="video-area video-bg pt-200 pb-200" data-background="{{asset('user/assets/img/service/video-bg.jpg')}}">
-        <div class="container">
-            <div class="row">
-                <div class="col-xl-12">
-                    <div class="video-caption text-center">
-                        <div class="video-icon">
-                            <a class="popup-video" href="https://www.youtube.com/watch?v=1aP-TXUpNoU" tabindex="0"><i
-                                    class="fas fa-play"></i></a>
-                        </div>
-                        <p class="pera1">Love where you're going in the perfect time</p>
-                        <p class="pera2">Tripo is a World Leading Online</p>
-                        <p class="pera3"> Tour Booking Platform</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    {{--<div class="video-area video-bg pt-200 pb-200" data-background="{{asset('user/assets/img/service/video-bg.jpg')}}">--}}
+        {{--<div class="container">--}}
+            {{--<div class="row">--}}
+                {{--<div class="col-xl-12">--}}
+                    {{--<div class="video-caption text-center">--}}
+                        {{--<div class="video-icon">--}}
+                            {{--<a class="popup-video" href="https://www.youtube.com/watch?v=1aP-TXUpNoU" tabindex="0"><i--}}
+                                    {{--class="fas fa-play"></i></a>--}}
+                        {{--</div>--}}
+                        {{--<p class="pera1">Love where you're going in the perfect time</p>--}}
+                        {{--<p class="pera2">Tripo is a World Leading Online</p>--}}
+                        {{--<p class="pera3"> Tour Booking Platform</p>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
     <!-- Video Start End -->
     <!-- Support Company Start-->
-    <div class="support-company-area support-padding fix">
+    {{--<div class="support-company-area support-padding fix">--}}
+        {{--<div class="container">--}}
+            {{--<div class="row align-items-center">--}}
+                {{--<div class="col-xl-6 col-lg-6">--}}
+                    {{--<div class="support-location-img mb-50">--}}
+                        {{--<img src="{{asset('user/assets/img/service/support-img.jpg')}}" alt="">--}}
+                        {{--<div class="support-img-cap">--}}
+                            {{--<span>Since 1992</span>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+                {{--<div class="col-xl-6 col-lg-6">--}}
+                    {{--<div class="right-caption">--}}
+                        {{--<!-- Section Tittle -->--}}
+                        {{--<div class="section-tittle section-tittle2">--}}
+                            {{--<span>About Our Company</span>--}}
+                            {{--<h2>We are Go Trip <br>Ravels Support Company</h2>--}}
+                        {{--</div>--}}
+                        {{--<div class="support-caption">--}}
+                            {{--<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor--}}
+                                {{--incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud</p>--}}
+                            {{--<div class="select-suport-items">--}}
+                                {{--<label class="single-items">Lorem ipsum dolor sit amet--}}
+                                    {{--<input type="checkbox" checked="checked active">--}}
+                                    {{--<span class="checkmark"></span>--}}
+                                {{--</label>--}}
+                                {{--<label class="single-items">Consectetur adipisicing sed do--}}
+                                    {{--<input type="checkbox" checked="checked active">--}}
+                                    {{--<span class="checkmark"></span>--}}
+                                {{--</label>--}}
+                                {{--<label class="single-items">Eiusmod tempor incididunt--}}
+                                    {{--<input type="checkbox" checked="checked active">--}}
+                                    {{--<span class="checkmark"></span>--}}
+                                {{--</label>--}}
+                                {{--<label class="single-items">Ad minim veniam, quis nostrud.--}}
+                                    {{--<input type="checkbox" checked="checked active">--}}
+                                    {{--<span class="checkmark"></span>--}}
+                                {{--</label>--}}
+                            {{--</div>--}}
+                            {{--<a href="#" class="btn border-btn">About us</a>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+    <!-- Support Company End-->
+    <!-- Testimonial Start -->
+
+    <!-- Blog Area Start -->
+    <div class="home-blog-area section-padding2">
         <div class="container">
-            <div class="row align-items-center">
-                <div class="col-xl-6 col-lg-6">
-                    <div class="support-location-img mb-50">
-                        <img src="{{asset('user/assets/img/service/support-img.jpg')}}" alt="">
-                        <div class="support-img-cap">
-                            <span>Since 1992</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xl-6 col-lg-6">
-                    <div class="right-caption">
-                        <!-- Section Tittle -->
-                        <div class="section-tittle section-tittle2">
-                            <span>About Our Company</span>
-                            <h2>We are Go Trip <br>Ravels Support Company</h2>
-                        </div>
-                        <div class="support-caption">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud</p>
-                            <div class="select-suport-items">
-                                <label class="single-items">Lorem ipsum dolor sit amet
-                                    <input type="checkbox" checked="checked active">
-                                    <span class="checkmark"></span>
-                                </label>
-                                <label class="single-items">Consectetur adipisicing sed do
-                                    <input type="checkbox" checked="checked active">
-                                    <span class="checkmark"></span>
-                                </label>
-                                <label class="single-items">Eiusmod tempor incididunt
-                                    <input type="checkbox" checked="checked active">
-                                    <span class="checkmark"></span>
-                                </label>
-                                <label class="single-items">Ad minim veniam, quis nostrud.
-                                    <input type="checkbox" checked="checked active">
-                                    <span class="checkmark"></span>
-                                </label>
-                            </div>
-                            <a href="#" class="btn border-btn">About us</a>
-                        </div>
+            <!-- Section Tittle -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="section-tittle text-center">
+                        <span>Our Recent news</span>
+                        <h2>Tourist Blog</h2>
                     </div>
                 </div>
             </div>
+            <div class="row">
+                <?php foreach ($results as $key =>$value)
+                {
+                ?>
+                <div class="col-xl-6 col-lg-6 col-md-6">
+                    <div class="home-blog-single mb-30">
+                        <div class="blog-img-cap">
+                            <div class="blog-img">
+                                <img src="{{asset('upload/post/'.$value->photo)}}" alt="<?=$value->title?>">
+                            </div>
+                            <div class="blog-cap">
+                                <p> | Traveling</p>
+                                <h3><a href="#"><?=$value->title?></a></h3>
+                                <a href="<?=url('/blog/'.$value->slug)?>" class="more-btn">Read more »</a>
+                            </div>
+                        </div>
+                        <div class="blog-date text-center">
+                            <span><?=date('d',strtotime($value->date_posted))?></span>
+                            <p><?=date('M',strtotime($value->date_posted))?></p>
+                        </div>
+                    </div>
+                </div>
+                <?php } ?>
+
+            </div>
         </div>
     </div>
-    <!-- Support Company End-->
-    <!-- Testimonial Start -->
+    <!-- Blog Area End -->
     <!-- Testimonial Start -->
     <div class="testimonial-area testimonial-padding" data-background="{{asset('user/assets/img/testmonial/testimonial_bg.jpg')}}">
         <div class="container ">
@@ -260,47 +301,6 @@
         </div>
     </div>
     <!-- Testimonial End -->
-    <!-- Blog Area Start -->
-    <div class="home-blog-area section-padding2">
-        <div class="container">
-            <!-- Section Tittle -->
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="section-tittle text-center">
-                        <span>Our Recent news</span>
-                        <h2>Tourist Blog</h2>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <?php foreach ($results as $key =>$value)
-                {
-                ?>
-                <div class="col-xl-6 col-lg-6 col-md-6">
-                    <div class="home-blog-single mb-30">
-                        <div class="blog-img-cap">
-                            <div class="blog-img">
-                                <img src="{{asset('upload/post/'.$value->photo)}}" alt="<?=$value->title?>">
-                            </div>
-                            <div class="blog-cap">
-                                <p> | Traveling</p>
-                                <h3><a href="#"><?=$value->title?></a></h3>
-                                <a href="<?=url('/blog/'.$value->slug)?>" class="more-btn">Read more »</a>
-                            </div>
-                        </div>
-                        <div class="blog-date text-center">
-                            <span><?=date('d',strtotime($value->date_posted))?></span>
-                            <p><?=date('M',strtotime($value->date_posted))?></p>
-                        </div>
-                    </div>
-                </div>
-                <?php } ?>
-
-            </div>
-        </div>
-    </div>
-    <!-- Blog Area End -->
-
 </main>
 
 
